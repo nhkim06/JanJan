283d13d12a5a44fda7ec0e7aa726de5c1dc85215
Nahyun <65719936+nhkim06@users.noreply.github.com>
2026-05-17 04:49:08 +0900

    fix : pwa deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from '@vue/plugin-vue'
+import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { VitePWA } from 'vite-plugin-pwa' // 👈 1. PWA 플러그인 임포트 추가
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
